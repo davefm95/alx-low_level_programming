badd1fc96bf6743b8a8ee0b3c54cc870bfd6d747
Femi Adetoye <davefm95@gmail.com>
2022-10-13 02:06:23 -0700

    correction to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-main()
+/**
+ *main -  Prints ""Programming is like building a multilingual puzzle"
+ *
+ *Return: 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
