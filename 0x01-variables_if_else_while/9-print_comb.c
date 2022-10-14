c423176ed2ca4364f4c8221c99eaeb82916a0112
Femi Adetoye <davefm95@gmail.com>
2022-10-14 07:21:59 -0700

    correction to 9-print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,17 @@
 int main(void)
 {
 	int i;
-	int j;
 	int ac1;
-	int ac2;
 
 	for (i = 1, ac1 = 48; i < 11; i++)
 	{
-		for (j = 1, ac2 = 48; j < 11; j++)
+		putchar(ac1);
+		if (i < 10)
 		{
-			putchar(ac1);
-			putchar(ac2);
 			putchar(',');
-			ac2++;
+			putchar(' ');
+			ac1++;
 		}
-		ac1++;
 	}
 	putchar('\n');
 	return (0);
