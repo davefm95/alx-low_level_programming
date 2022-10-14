05715b94ee6f8cdd316955162733a312b46016d0
Femi Adetoye <davefm95@gmail.com>
2022-10-14 11:48:45 -0700

    cd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,27 +17,25 @@ int main(void)
 	{
 		for (j = 0, ac2 = 48; j < 10; j++, ac2++)
 		{
-			if (ac2 > ac1)
+			for (k = 0, ac3 = 48; k < 10; k++, ac3++)
 			{
-				for (k = 0, ac3 = 48; k < 10; k++, ac3++)
+				if ((ac2 > ac1) && (ac3 > ac2))
 				{
-					if (ac3 > ac2)
+					putchar(ac1);
+					putchar(ac2);
+					putchar(ac3);
+					if ((i == 7) && (j == 8) && (k == 9))
 					{
-						putchar(ac1);
-						putchar(ac2);
-						putchar(ac3);
-						if ((i == 7) && (j == 8) && (k == 9))
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
+
 		}
 	}
 	putchar('\n');
