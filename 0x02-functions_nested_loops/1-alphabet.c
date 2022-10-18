70fa0a3f3256eeab5cef277d6124a4c214e3f30e
Femi Adetoye <davefm95@gmail.com>
2022-10-18 10:50:05 -0700

    correction to 1-alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "_putchar.h"
+/**
+ *print_alphabet - prints the a-z in lowercase
+ */
 void print_alphabet(void)
 {
-        char ch = 'a';
-        int i = 1;
-        for (; i < 27; i++)
-        {
-                _putchar(ch);
-                ch++;
-        }
-        _putchar('\n');
+	char ch = 'a';
+	int i = 1;
+	for (; i < 27; i++)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
 }
