d5aa5c3cbb769dd1b20ff6b45b8377819e992cee
Femi Adetoye <davefm95@gmail.com>
2022-10-18 16:22:44 -0700

    added function that orints time tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *print_times_table - prints times tabke of a specified number
+ *@n: specified integer
+ */
+void print_times_table(int n)
+{
+	int i, j, s;
+
+	if (n > 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				s = i * j;
+				printf("%d", s);
+				if (j != n)
+				{
+					if (s + i < 10)
+						printf(",   ");
+					else if (s + i < 100)
+						printf(",  ");
+					else
+						printf(", ");
+				}
+				else
+					printf("\n");
+			}
+		}
+	}
+}
