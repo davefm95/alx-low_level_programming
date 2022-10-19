4a7e513ab39f7900b9f5036c1780700e1a03ed39
Femi Adetoye <davefm95@gmail.com>
2022-10-18 17:14:39 -0700

    added code thatbprints fibonacci sequence of numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - prints first 50 numbers of a fibonacci sequence starting with 1 and 2
+ *
+ *Return: returns 0 on completion
+ */
+int main(void)
+{
+	long int n1 = 1;
+	long int n2 = 2;
+	int i = 1;
+
+	printf("%li, %li, ", n1, n2);
+	for (; i <= 48; i++)
+	{
+		if (i % 2 != 0)
+		{
+			n1 += n2;
+			printf("%li", n1);
+		}
+		else
+		{
+			n2 += n1;
+			printf("%li", n2);
+		}
+		if (i < 50)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
