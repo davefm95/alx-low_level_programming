c779c1516db2c1dc889d26ce47865d9dc6509435
Femi Adetoye <davefm95@gmail.com>
2022-10-20 06:56:31 -0700

    added code that prints highest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - prints largest prime factor of 612852475143
+ *Return: Always 0
+ */
+int main(void)
+{
+	long long int num = 612852475143;
+	long long int hpf;
+	long long int i = 2;
+
+	while (i < num)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+		}
+		else
+		{
+			if (i == num - 1)
+				hpf = num;
+			i++;
+		}
+	}
+	printf("%lld\n", hpf);
+
+	return (0);
+}
