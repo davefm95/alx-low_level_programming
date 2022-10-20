d865a335396a6ba0c288697fd80cd676f49c1f5c
Femi Adetoye <davefm95@gmail.com>
2022-10-20 10:28:05 -0700

    unauto

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void print_remaining_days(int day, int month, int year);
+
 int largest_number(int a, int b, int c);
+
+void print_remaining_days(int day, int month, int year);
+
+int convert_day(int, int);
+
 #endif
