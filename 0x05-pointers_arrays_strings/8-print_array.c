5f78ebfdfcb79f35c42e058533ecfb8eca6e3c36
Femi Adetoye <davefm95@gmail.com>
2022-10-24 03:50:56 -0700

    added code that orints n elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *print_array - prints a given number of elements in an array
+ *@a: arrau pointer variable
+ *@n: number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i < n - 1)
+			printf(", ");
+	}
+}
