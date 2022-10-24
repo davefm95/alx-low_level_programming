bb51e4d3496597fc41ce68e39d9462e97750b4c0
Femi Adetoye <davefm95@gmail.com>
2022-10-24 00:50:16 -0700

    added function that peints out a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: string pointer variable
+ */
+void _puts(char *str)
+{
+	int i;
+	
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
