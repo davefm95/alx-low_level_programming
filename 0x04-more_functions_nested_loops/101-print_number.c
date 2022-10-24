20d7852f45676b0f4aaae6ad45f65b60710f4c13
Femi Adetoye <davefm95@gmail.com>
2022-10-23 17:18:23 -0700

    correction of 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,31 @@
 #include "main.h"
+#include <math.h>
 /**
  *print_number - prints an integer
  *@n: integer argument
  */
 void print_number(int n)
 {
-	int a, b, c, d, e;
+	if (n >= -2147483647 && n <= 2147483647)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	a = n / 10000;
-	b = (n % 10000) / 1000;
-	c = (n % 1000) / 100;
-	d = (n % 100) / 10;
-	e = n % 10;
-	if (n < 100)
-	{
-		if (d != 0)
-			_putchar(d + '0');
-		_putchar(e + '0');
-	}
-	else if (n < 1000)
-	{
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar(e + '0');
-	}
-	else if (n < 10000)
-	{
-		_putchar(b + '0');
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar(e + '0');
-	}
-	else if (n < 100000)
-	{
-		_putchar(a + '0');
-		_putchar(b + '0');
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar(e + '0');
+		/*initialize variable sum to find n's power of 10*/
+		int sum = 0;
+		int i;
+
+		for (i = n; i >= 10; i / 10)
+			sum++;
+		for (i = pow(10, sum); i >= 10; i / 10)
+		{
+			_putchar(n / i + '0');
+			n = n % i;
+			if (i == 10)
+				_putchar(n + '0');
+		}
 	}
 }
