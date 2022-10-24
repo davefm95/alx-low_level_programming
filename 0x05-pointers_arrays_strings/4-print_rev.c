9be06c99b2220a487299d9deabb0587c3a4c0b61
Femi Adetoye <davefm95@gmail.com>
2022-10-24 01:04:13 -0700

    added function that prints tringbin reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - prints a string in reverse
+ *@s: string pointer variable
+ */
+void print_rev(char *s)
+{
+	int ch;
+	int i;
+	int sl;
+
+	ch = *s;
+	sl = 0;
+	while (ch)
+	{
+		sl++;
+		ch = *(s + sl);
+	}
+	for (i = sl; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
