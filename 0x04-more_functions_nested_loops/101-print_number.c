8778710419fda88f630a85b752a43a539f9c2a83
Femi Adetoye <davefm95@gmail.com>
2022-10-23 17:30:54 -0700

    correction to 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,10 @@
  */
 void print_number(int n)
 {
+	/*initialize variable sum to find n's power of 10*/
+	int sum;
+	int i;
+
 	if (n >= -2147483647 && n <= 2147483647)
 	{
 		if (n < 0)
@@ -13,14 +17,10 @@ void print_number(int n)
 			_putchar('-');
 			n = -n;
 		}
-
-		/*initialize variable sum to find n's power of 10*/
-		int sum = 0;
-		int i;
-
-		for (i = n; i >= 10; i / 10)
+		sum = 0;
+		for (i = n; i >= 10; i /= 10)
 			sum++;
-		for (i = pow(10, sum); i >= 10; i / 10)
+		for (i = pow(10, sum); i >= 10; i /= 10)
 		{
 			_putchar(n / i + '0');
 			n = n % i;
