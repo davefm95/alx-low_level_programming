cd83510e3c671b2a65cd93f85c86b1206ed4ca23
Femi Adetoye <davefm95@gmail.com>
2022-10-24 03:32:33 -0700

    a1;2cdded

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *puts_half - printds half of a string
+ *@str: string pointer variable
+ */
+void puts_half(char *str)
+{
+	int i;
+	int sl;
+	char c;
+	int hl;
+
+	sl = 0;
+	c = *str;
+	while (c)
+	{
+		sl++;
+		c = *(str + sl);
+	}
+	if (sl % 2 == 0)
+		hl = sl / 2;
+	else
+		hl = sl / 2 + 1;
+	for (i = hl; i < sl; i++)
+		_putchar(*(str + i));
+}
