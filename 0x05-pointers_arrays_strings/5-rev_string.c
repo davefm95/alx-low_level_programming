bf3f5734fe7022f0e40cbe5201984d9011d50cfc
Femi Adetoye <davefm95@gmail.com>
2022-10-24 19:25:46 -0700

    ye

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,25 @@ void rev_string(char *s)
 {
 	int sl;
 	int i;
+	int j;
+	int k;
 	char ch;
 	char *rev;
-	char *tmp;
+	char cho;
 
 	sl = 0;
 	ch = *s;
 	rev = s;
-	tmp = s;
 	while (ch)
 	{
 		sl++;
 		ch = *(s + sl);
 	}
-	for (i = sl - 1; i >= 0; i--, tmp++)
+	for (i = 0, j = sl - 1, k = 0; i < sl / 2; i++, j--, k++)
 	{
-		tmp = (rev + i);
+		cho = *(rev + j);
+		*(rev + j) = *(s + k);
+		*(s + k) = cho;;
 	}
-	s = tmp;
+
 }
