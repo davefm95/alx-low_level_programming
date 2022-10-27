714d077da9917301e2ba46e77005c3eec52afa1f
Femi Adetoye <davefm95@gmail.com>
2022-10-27 03:27:15 -0700

    added code that converta string chars to uppercasec

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ *string_toupper - changes lowercaae character to uppercaae
+ *@s: string to be modified
+ *Return: returns modified string
+ */
+char *string_toupper(char *s)
+{
+	int i, c;
+	char ch;
+
+	for (i = 0; s[i]; i++)
+	{
+		c = s[i];
+		if (c >= 97 && c <= 122)
+		{
+			c -=32;
+			ch = c;
+			s[i] = ch;
+		}
+	}
+	return (s);
+}
