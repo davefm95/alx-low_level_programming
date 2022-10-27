afa5989f2d7d7323aafd0fd3ecf5ee841a88546f
Femi Adetoye <davefm95@gmail.com>
2022-10-26 23:39:30 -0700

    correct

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,36 +9,38 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int stlen1, i, stlen2, nwstlen;
-	static char *ptr;
-	char *dest2, *src2;
+	int stlen1, i, stlen2, nwstlen, j;
+	char *ptr;
 
 	stlen1 = 0;
 	stlen2 = 0;
-	dest2 = dest;
-	src2 = src;
-	while (*dest2)
+	while (*dest)
 	{
 		stlen1++;
-		dest2++;
+		dest++;
 	}
-	while (*src2)
+	dest = dest - stlen1;
+	printf("dest is %s", dest);
+	while (*src)
 	{
 		stlen2++;
-		src2++;
+		src++;
 	}
+	src = src - stlen2;
 	nwstlen = (stlen1 + stlen2) + 1;
 	ptr = malloc(nwstlen * sizeof(*ptr));
 	for (i = 0; i < nwstlen - 1; i++)
 	{
 		if (i < stlen1)
 			ptr[i] = dest[i];
-		else
-		{
-			ptr[i] = src[i - stlen1];
-		}
+	}
+	for (j = stlen1, i = 0; j < nwstlen - 1; j++, i++)
+	{
+		if (src[i])
+			ptr[j] = src[i];
 	}
 	ptr[i] = '\0';
-	dest = ptr;
+	for (; stlen1 < nwstlen; stlen1++)
+		dest[stlen1] = ptr[stlen1];
 	return (dest);
 }
