af07e326622dcc63636e1aa4b31a1cc482301a2a
Femi Adetoye <davefm95@gmail.com>
2022-10-31 08:12:12 -0700

    added correction to  8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,19 +4,17 @@
  *@a: matrix array
  *@size: size of array
  */
-void print_diagsums(int *a[], int size)
+void print_diagsums(int *a, int size)
 {
-	int i, j, sumlr = 0, sumrl = 0, lstArrEle = size - 1;
+	int square_size = size * size;
+	int i, sumlr = 0, sumrl = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < square_size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				sumlr += a[i][j];
-			if (i + j == lstArrEle - 1)
-				sumrl += a[i][j];
-		}
+		if (i % (size + 1) == 0)
+			sumlr += a[i];
+		if (i % (size - 1) == 0)
+			sumrl += a[i];
 	}
 	printf("%d, %d", sumlr, sumrl);
 }
