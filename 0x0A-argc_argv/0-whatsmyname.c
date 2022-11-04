7952a87bd843e55c857e545bfec0fcaf1947f84c
Femi Adetoye <davefm95@gmail.com>
2022-11-03 23:40:23 -0700

    added file that prints out its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints iut the name of the file
+ *@argc: number of arguments
+ *@argv: pointer to array of arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("./%s", *arg);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+This file contains information about the 0x0A-argc_argv
+0.The 0-whatsmyname.c file pronts out the name of the file
