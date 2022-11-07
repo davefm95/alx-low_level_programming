f861479ceb123c05de91c83c573c6a35cc53923b
Femi Adetoye <davefm95@gmail.com>
2022-11-07 04:23:33 -0800

    added file that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ *_strdup - copirs a string into memeory and returns pointer to copy
+ *@str: string to br copied
+ *Return: returns pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	char *strdup;
+	int i, strlen = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[strlen])
+		strlen++;
+	strdup = malloc(strlen * sizeof(*strdup));
+	if (strdup == NULL)
+		return (NULL);
+	for (i = 0; i < strlen; i++)
+		strdup[i] = str[i];
+	return (strdup);
+}
