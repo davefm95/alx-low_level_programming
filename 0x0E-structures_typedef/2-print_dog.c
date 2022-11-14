9efe34da001310ccf55c27fcf942a3332d7e925e
Femi Adetoye <davefm95@gmail.com>
2022-11-14 14:43:31 -0800

    y

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	char *nil = "(nil)";
 
 	if (d == NULL)
-		exit(0);
+		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : nil);
 	if (d->age <= 0)
 		printf("Age: %s\n", nil);
