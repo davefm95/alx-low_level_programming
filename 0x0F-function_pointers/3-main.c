c1f6b0bb313b0956df07f9d758e38d190abd92f0
Femi Adetoye <davefm95@gmail.com>
2022-11-16 14:53:08 -0800

    g1;2c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,29 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result, num1, num2, i;
 	op_t opt;
+	char *o[] = {"+", "-", "*", "/", "%"};
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	opt.op = argv[2];
-	opt.f = get_op_func(opt.op);
-	if (opt.f == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((strcmp(opt.op, "/") == 0 || strcmp(opt.op, "%") == 0) && num2 == 0)
+	opt.op = argv[2];
+	for (i = 0; i < 5; i++)
 	{
-		printf("Error\n");
-		exit(100);
+		if (strcmp(opt.op, o[i]) == 0)
+		{
+			if ((strcmp(opt.op, "/") == 0 || strcmp(opt.op, "%") == 0) && num2 == 0)
+			{
+				printf("Error\n");
+				exit(100);
+			}
+			opt.f = get_op_func(opt.op);
+			result = opt.f(num1, num2);
+			printf("%d\n", result);
+			return (0);
+		}
 	}
-	result = opt.f(num1, num2);
-	printf("%d\n", result);
-	return (0);
+	printf("Error\n");
+	exit(99);
 }
