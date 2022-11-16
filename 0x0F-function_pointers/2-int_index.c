f40850ae5cc07c31f09404b045b9ecd3c3c6d978
Femi Adetoye <davefm95@gmail.com>
2022-11-15 23:25:03 -0800

    added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ *int_index - searches for an integer
+ *@array: array to be searched
+ *@size: size of array
+ *@cmp: pointer to function thats doin the searching
+ *Return: index of matching elemnt or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,4 +1,4 @@
 This file contains information on tye solutions to the 0x0F-function_pointers tasks
 0.The 0-print_name.c function prints a name
 1.The 1-array_iterator.c file performs a function on every element of an array
-2.
+2.The  2-int_index.c file searches for an integer.
