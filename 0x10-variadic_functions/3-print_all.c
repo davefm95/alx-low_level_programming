256c0bc6bae7c9c12df2456d57f919eb867b1ba3
Femi Adetoye <davefm95@gmail.com>
2022-11-17 14:37:19 -0800

    g

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,37 +7,36 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
+	int i = 0, fswitch = 0, j;
 	va_list p;
-	char *s;
+	char *s, far[] = {'c', 'i', 's', 'f'};
 
 	va_start(p, format);
 	while (format && format[i])
 	{
+		j = 0;
+		while (j < 4)
+		{
+			if (fswitch && format[i] == far[j])
+				printf(", ");
+			j++;
+		}
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(p, int));
+			printf("%c", va_arg(p, int)), fswitch++;;
 			break;
 		case 'i':
-			printf("%d", va_arg(p, int));
+			printf("%d", va_arg(p, int)), fswitch++;
 			break;
 		case 's':
 			s = va_arg(p, char *);
 			if (s == NULL)
 				s = "(nil)";
-			printf("%s", s);
+			printf("%s", s), fswitch++;
 			break;
 		case 'f':
-			printf("%f", va_arg(p, double));
-		}
-		switch (format[i + 1])
-		{
-		case 'c':
-		case 'i':
-		case 's':
-		case 'f':
-			printf(", ");
+			printf("%f", va_arg(p, double)), fswitch++;
 		}
 		i++;
 	}
