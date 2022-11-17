82b77c520f5615aad4524ab1b7a5614537974756
Femi Adetoye <davefm95@gmail.com>
2022-11-17 13:45:42 -0800

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,40 +12,36 @@ void print_all(const char * const format, ...)
 	char *s;
 	float f;
 
-	if (format)
+	va_start(p, format);
+	while (format && format[i])
 	{
-		va_start(p, format);
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", va_arg(p, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(p, int));
-				break;
-			case 's':
-				s = va_arg(p, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
-				break;
-			case 'f':
-				f = (float)va_arg(p, double);
-				printf("%f", f);
-			}
-			switch (format[i + 1])
-			{
-			case 'c':
-			case 'i':
-			case 's':
-			case 'f':
-				printf(", ");
-			}
-			i++;
+		case 'c':
+			printf("%c", va_arg(p, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(p, int));
+			break;
+		case 's':
+			s = va_arg(p, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			break;
+		case 'f':
+			printf("%f", va_arg(p, double));
 		}
+		switch (format[i + 1])
+		{
+		case 'c':
+		case 'i':
+		case 's':
+		case 'f':
+			printf(", ");
+		}
+		i++;
+	}
 	printf("\n");
 	va_end(p);
-	}
 }
