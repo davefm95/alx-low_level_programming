355b6a08a96363f8f0b17064454b2a49e53f3706
Femi Adetoye <davefm95@gmail.com>
2022-11-28 07:49:14 -0800

    added 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,7 @@ size_t print_listint(const listint_t *h)
 		return (0);
 	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%u] %s\n", 0, "(nil)");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		num_nodes++;
 		h = h->next;
 	}
