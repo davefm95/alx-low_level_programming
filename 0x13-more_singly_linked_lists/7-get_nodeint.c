f2941baa718c498960d7c8da7e4413ee276fcb06
Femi Adetoye <davefm95@gmail.com>
2022-11-28 10:11:30 -0800

    added correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *reqnode;
+	listint_t *reqnode = NULL;
 
 	if (head == NULL)
 		return (NULL);
