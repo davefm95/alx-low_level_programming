bf06ca626f9e33b330953e200d616561899dd542
Femi Adetoye <davefm95@gmail.com>
2022-12-01 15:21:14 -0800

    yes

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,6 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > sizeof(*n) * 8 - 1)
 		return (-1);
 	numatin = 1 << index;
-	*n = (n & ~numatin) | (1 << index);
+	*n = (*n & ~numatin) | (1 << index);
 	return (1);
 }
