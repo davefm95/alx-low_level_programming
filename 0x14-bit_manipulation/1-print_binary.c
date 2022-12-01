52ed7d9b891ae586bbb49c226b63899659d33e9e
Femi Adetoye <davefm95@gmail.com>
2022-12-01 14:18:13 -0800

    added 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *print_binary - prints binary rep of nos
+ *@n: number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int b = 1;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	if (n == 1)
+	{
+		printf("1");
+		return;
+	}
+	for (; b * 2 <= n; b *= 2);
+	while (b > 0)
+	{
+		if (b <= n)
+		{
+			printf("1");
+			n -= b;
+			b >>= 1;
+			continue;
+		}
+		printf("0");
+		b >>= 1;
+	}
+}
