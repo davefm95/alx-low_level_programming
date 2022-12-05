b2c6be0f26d07a7fa8c39d78b446e1cf1ba7874a
Femi Adetoye <davefm95@gmail.com>
2022-12-05 02:31:22 -0800

    added 0read txt file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *read_textfile - reads text file and prints to stdout
+ *@filename: name of txt file
+ *@letters: number of letters it should print
+ *Return: num of letters printed and read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t charsread, charswrit;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(letters + 1);
+	charsread = read(fd, buf, letters);
+	if (charsread <= 0)
+		return (0);
+	buf[letters + 1] = '\0';
+	charswrit = write(STDOUT_FILENO, buf, charsread);
+	if (charswrit <= 0)
+		return (0);
+	return (charswrit);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+this file contains info about the 0x15-file_io projext
+0.The 0-read_textfile.c reads a text file and prinys it to the posix standard output
+1.
