c58826a82e0af9b16081d5866b375e55bb9ff938
Femi Adetoye <davefm95@gmail.com>
2023-01-12 06:43:12 -0800

    addee 0 and 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,6 +7,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num_nodes = 0;
+
 	if (!h)
 		return (0);
 	while (h)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,6 +7,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t num = 0;
+
 	if (!h)
 		return (0);
 	while (h)
